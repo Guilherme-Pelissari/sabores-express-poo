f0bb48b80c481ca06eed3c09d9d5a191d3a9fa74
guilhermefiap <guipelissari2000@gmail.com>
2025-08-11 19:31:29 -0300

    adcionando camadas de protecao nas variaveis

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -2,18 +2,18 @@ class Restaurante:
     restaurantes = []
 
     def __init__(self, nome, categoria):
-        self.nome = nome
-        self.categoria = categoria
+        self._nome = nome.title()
+        self._categoria = categoria.upper()
         self._ativo = False
         Restaurante.restaurantes.append(self)
 
     def __str__(self):
-        return f'{self.nome} | {self.categoria}'
-    
+        return f'{self._nome} | {self._categoria}'
+     
     def listar_restaurantes():
         print(f"{'Nome do restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Status'}")
         for restaurante in Restaurante.restaurantes:
-            print(f'{restaurante.nome.ljust(25)} | {restaurante.categoria.ljust(25)} | {restaurante.ativo}')
+            print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {restaurante.ativo}')
 
     @property
     def ativo(self):
@@ -21,6 +21,7 @@ class Restaurante:
 
 
 restaurante_praca = Restaurante('Praça', 'gourmet')
+
 restaurante_pizza = Restaurante('Pizza express', 'Italiana')
 
 Restaurante.listar_restaurantes()
