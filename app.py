299127096b56b256737bfe709560d6c266a568f6
guilhermefiap <guipelissari2000@gmail.com>
2025-08-11 21:34:39 -0300

    criando o main

---
+++ app.py
@@ -0,0 +1,13 @@
+from modelos.restaurante import Restaurante
+
+restaurante_praca = Restaurante('praça', 'gourmet')
+restaurante_mexicano = Restaurante('Mexican food', 'Mexicana')
+restaurante_japones = Restaurante('Japa', 'Japonesa')
+
+restaurante_mexicano.alternar_estado()
+
+def main():
+    Restaurante.listar_restaurantes()
+
+if __name__ == '__main__':
+    main()
