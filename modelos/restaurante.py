62f316197170b453b1ad9bc8f997c39dea23e463
guilhermefiap <guipelissari2000@gmail.com>
2025-08-20 22:07:07 -0300

    feat: implementa metodos para adicionar bebidas e pratos

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -18,8 +18,9 @@ class Restaurante:
         self._nome = nome.title()  
         self._categoria = categoria.upper() 
         self._ativo = False 
-        self._avaliacao = [] 
-        Restaurante.restaurantes.append(self)  
+        self._avaliacao = []
+        self._cardapio = []
+        Restaurante.restaurantes.append(self)   
 
     def __str__(self):
         """
@@ -75,3 +76,9 @@ class Restaurante:
         quantidade_de_notas = len(self._avaliacao)
         media = round(soma_das_notas / quantidade_de_notas, 1)  # Calcula a média e arredonda para uma casa decimal
         return media
+    
+    def adicionar_bebida_no_cardapio(self,bebida):
+        self._cardapio.append(bebida)
+    
+    def adicionar_prato_no_cardapio(self,prato):
+        self._cardapio.append(prato)
